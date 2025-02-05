4c43c207cefbd4f7ebdc5f38fbf8576cb5ec475f
Raj Singh <140183009+rajsingh565@users.noreply.github.com>
2025-02-05 20:08:19 +0530

    Time: 45 ms (28.74%), Space: 18.2 MB (23.46%) - LeetHub

---
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.py
@@ -0,0 +1,6 @@
+class Solution:
+    def deleteNode(self, node):
+        # Overwrite data of next node on current node.
+        node.val = node.next.val
+        # Make current node point to next of next node.
+        node.next = node.next.next
