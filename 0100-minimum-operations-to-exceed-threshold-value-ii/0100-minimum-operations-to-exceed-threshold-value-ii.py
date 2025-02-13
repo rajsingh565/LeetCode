ae8cdfe08596780a94b2dbe8782036d5272b1b84
Raj Singh <140183009+rajsingh565@users.noreply.github.com>
2025-02-13 23:09:47 +0530

    Time: 1332 ms (62.5%), Space: 23.2 MB (68.75%) - LeetHub

---
+++ 0100-minimum-operations-to-exceed-threshold-value-ii/0100-minimum-operations-to-exceed-threshold-value-ii.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minOperations(self, nums, k):
+        heapq.heapify(nums)
+        op = 0
+
+        while nums[0] < k:
+            x = heapq.heappop(nums)
+            y = heapq.heappop(nums)
+            heapq.heappush(nums, x * 2 + y)
+            op += 1
+
+        return op
